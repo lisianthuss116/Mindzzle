6f6f8c8c47f525328b54d03bd8ed7791b5b7eb83
lisianthus <lisianthus623@gmail.com>
2020-02-14 21:59:03 +0700

    adding new urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import (Home, ProductDetail)
+from .views import (Home, ProductDetail, add_to_cart, remove_from_cart)
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Home.as_view(), name='homes'),
     path('product/<slug>', ProductDetail.as_view(), name='product'),
-    path('', Home.as_view(), name='homes'),
+    path('add-to-cart/<slug>', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>', remove_from_cart, name='remove-from-cart'),
 ]
