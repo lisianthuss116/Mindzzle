da86ddc7e6fccff4ec89ea98057db1926432c9a4
lisianthus <lisianthus623@gmail.com>
2020-02-14 14:14:03 +0700

    create item-list view

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+
+def item_list(request):
+    '''
+    showing all item list
+
+    :param request
+    :return: render item-list
+    '''
+    context = {
+        'items' : Item.objects.all()
+    }
+
+    return render(request, 'core/item_list.html', context)
