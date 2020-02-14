a18936f39779c0e36e37648d35cb6b95590499e7
lisianthus <lisianthus623@gmail.com>
2020-02-14 17:00:42 +0700

    use class-based-view

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
-
-def item_list(request):
+class Home(ListView) :
     '''
     showing all item list
 
     :param request
     :return: render item-list
     '''
-    context = {
-        'items': Item.objects.all()
-    }
+    model = Item
+    template_name = 'core/home-page.html'
 
-    return render(request, 'core/item_list.html', context)
+class ProductDetail(DetailView) :
+    '''
+    showing specific product item
+
+    :param request
+    :return: all products
+    '''
+    model = Item
+    template_name = 'core/product.html'
+
+def checkout(request):
+    '''
+    checkout
+
+    :param request
+    '''
+    return render(request, 'core/checkout.html', context)
