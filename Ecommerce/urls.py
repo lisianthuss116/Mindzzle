bd97ac3634e1a6b4f79c9560b75d614609dfca05
lisianthus <lisianthus623@gmail.com>
2020-02-15 18:18:50 +0700

    add new urls and static setting

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -1,10 +1,16 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from account.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Urls core
-    path('', include('core.urls', namespace='core'))
+    # core
+    path('', include('core.urls', namespace='core')),
+    # register
+    path('register/', register, name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
