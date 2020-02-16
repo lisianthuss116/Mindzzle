7592baef9b6104ea4266d4419da09f4f039a3495
lisianthus <lisianthus623@gmail.com>
2020-02-16 11:44:09 +0700

    change user field name

--- backend/account/models.py
+++ backend/account/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_account_name = models.OneToOneField(User, on_delete=models.CASCADE)
     user_image = models.ImageField(
         default='default.jpg', upload_to='user_image')
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user_account_name.username} Profile'
