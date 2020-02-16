0d4e9e44ec8d0736f3f98abcdd76ea939d1a504d
lisianthus <lisianthus623@gmail.com>
2020-02-16 11:46:58 +0700

    change url name

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,7 +4,7 @@ from .views import (Home, ProductDetail, add_to_cart, remove_from_cart)
 app_name = 'core'
 
 urlpatterns = [
-    path('', Home.as_view(), name='homes'),
+    path('', Home.as_view(), name='home'),
     path('product/<slug>', ProductDetail.as_view(), name='product'),
     path('add-to-cart/<slug>', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>', remove_from_cart, name='remove-from-cart'),
