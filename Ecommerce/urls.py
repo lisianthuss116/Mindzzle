927f2a7375a2c51caab988ad1829afc5bf27b65f
lisianthus <lisianthus623@gmail.com>
2020-02-16 13:18:48 +0700

    adding logout url

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -10,12 +10,14 @@ urlpatterns = [
     # core
     path('', include('core.urls', namespace='core')),
 
-    # AUTH
     # register
     path('register/', register, name='register'),
     # login
     path('login/', auth_view.LoginView.as_view(
         template_name='auth/login.html'), name='login'),
+    # logout
+    path('logout/', auth_view.LogoutView.as_view(
+        template_name='auth/logout.html'), name='logout'),
 ]
 
 if settings.DEBUG:
