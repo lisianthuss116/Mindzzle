70d6a52cfec41161e669a008060c2cfbffd1b2b1
lisianthus <lisianthus623@gmail.com>
2020-02-18 11:42:19 +0700

    change slug into uniq

--- backend/core/models.py
+++ backend/core/models.py
@@ -24,7 +24,7 @@ class Item(models.Model):
     discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True, unique=True)
 
     def __str__(self):
         return self.title
