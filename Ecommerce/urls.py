fde9ced35081acb8ec053ebadc88e5f058548493
lisianthus <lisianthus623@gmail.com>
2020-02-25 14:44:00 +0700

    add login url

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.conf.urls.static import static
-from account.views import (register, profile) #Remind me to include LoginView & LogoutView
+from account.views import (register, profile, login)
 from Ecommerce.api_routers import router
 from django.contrib.auth import views as auth_view
 from rest_framework.authtoken import views
+import re
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,15 +19,12 @@ urlpatterns = [
     # accounts
     path('profile/', profile, name='profile'),
     # login
-    path('login/', auth_view.LoginView.as_view(
-        template_name='auth/login.html'), name='login'),
+    path('login/', login, name='login'),
     # logout
     path('logout/', auth_view.LogoutView.as_view(
         template_name='auth/logout.html'), name='logout'),
     # api
     path('api/v1/', include(router.urls)),
-    # path('api/v1/auth/login', LoginView.as_view()),
-    # path('api/v1/auth/logout', LogoutView.as_view()),
     path('api-token-auth/', views.obtain_auth_token, name='api-token-auth')
 ]
 
