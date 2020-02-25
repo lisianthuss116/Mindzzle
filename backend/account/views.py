54d53aa772826356ca6890b174ca846324500182
lisianthus <lisianthus623@gmail.com>
2020-02-25 14:46:49 +0700

    adding login view and update register view

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,5 +1,5 @@
 from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
-from account.forms import UserRegistrationForm
+from account.forms import UserRegistrationForm, UserLoginForm
 from .models import Profile
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
@@ -9,10 +9,29 @@ from rest_framework.response import Response
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login as django_login, logout as django_logout
+from django.contrib.auth import authenticate, login as login_auth
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import admin
 from django.contrib import messages
+from account.services import *
+
+
+def activation(request, key):
+    activation_expired = False
+    already_active = False
+    profile = get_object_or_404(Profile, activation_key=key)
+    if profile.user.is_active == False:
+        if timezone.now() > profile.key_expires:
+            activation_expired = True
+            user_id = profile.user.id
+        else:
+            profile.user.is_active = True
+            profile.user.save()
+    else:
+        already_active = True
+
+    return render(request, 'account/activation.html', locals())
+
 
 @require_http_methods(['GET', 'POST'])
 def register(request):
@@ -28,9 +47,16 @@ def register(request):
         form = UserRegistrationForm(request.POST or None)
         # validate form
         if form.is_valid():
-            form.save()
-            # if data does not valid, return only username field
-            username = form.cleaned_data.get('username')
+            datas = {}
+            datas['email'] = form.cleaned_data['email']
+            datas['username'] = form.cleaned_data['username']
+            datas['password1'] = form.cleaned_data['password1']
+
+            username_salt = datas['username']
+            username_salt = username_salt.encode('utf-8')
+            datas['activation_key'] = activation_code(username_salt)
+
+            form.save(datas)
             # message [success]
             messages.success(request, 'Your account has been created!')
             # Redirect to login-page
@@ -39,11 +65,43 @@ def register(request):
     else:
         form = UserRegistrationForm()
 
+    return render(request, 'auth/register.html', {'form': form})
+
+
+@require_http_methods(['GET', 'POST'])
+def login(request):
+    """
+    user login
+
+    :param request:
+    :method POST:
+    :return render register page
+    """
+    # check request method
+    if request.method == "POST":
+        form = UserLoginForm(request.POST or None)
+        # validate form
+        if form.is_valid():
+            # if data does valid, get fields
+            email = form.cleaned_data.get('email')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(
+                email=email, username=username, password=password)
+            login_auth(request, user)
+            # message [success]
+            messages.success(request, 'Success login')
+            # Redirect to login-page
+            return redirect('/')
+    # no requested data
+    else:
+        form = UserLoginForm()
+
     context = {
         'form': form
     }
 
-    return render(request, 'auth/register.html', context)
+    return render(request, 'auth/login.html', context)
 
 
 @login_required
@@ -69,17 +127,3 @@ def profile(request):
         'p_form': p_form
     }
     return render(request, 'profile_page/profile.html', context)
-
-# class LoginView(APIView):
-#     def post(self, request):
-#         serializer = LoginSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.validated_data
-#         django_login(request, user)
-#         token, created = Token.objects.get_or_create(user=user)
-#         return Response({"token":token.key}, status=200)
-# class LogoutView(APIView):
-#     authentication_classes = (TokenAuthentication,)
-#     def post(self, request):
-#         django_logout(request)
-#         return Response(status=204)
