aad122653d26411a422e60b8cd737f0706c9c8f5
lisianthus <lisianthus623@gmail.com>
2020-02-25 14:46:04 +0700

    add new field

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-
+# import datetime
+from datetime import datetime, timedelta
 
 class Profile(models.Model):
     user_account_name = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,6 +10,9 @@ class Profile(models.Model):
         default='avatar.svg',
         upload_to='user_image')
     created_date = models.DateTimeField(auto_now_add=True)
+    activation_key = models.CharField(max_length=100, blank=True)
+    key_expires = models.DateField(default=datetime.strftime(
+        datetime.now()+timedelta(days=2), "%Y-%m-%d"), editable=False, max_length=20)
 
     def __str__(self):
         return f'{self.user_account_name.username} Profile'
