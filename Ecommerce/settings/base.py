e64ca8861cd090f44c2ae9af6bceeeda81a41ba6
lisianthus <lisianthus623@gmail.com>
2020-02-25 14:43:23 +0700

    change time zone

--- Ecommerce/settings/base.py
+++ Ecommerce/settings/base.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # installed app
+    # Local
     'account.apps.AccountConfig',
     'core.apps.CoreConfig',
     # third party
@@ -91,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
