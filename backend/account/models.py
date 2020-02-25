102e76f3b07745b4e040bdcaa1fea080edd78978
lisianthus <lisianthus623@gmail.com>
2020-02-25 15:04:49 +0700

    update models

--- backend/account/models.py
+++ backend/account/models.py
@@ -12,8 +12,7 @@ class Profile(models.Model):
         upload_to='user_image')
     created_date = models.DateTimeField(auto_now_add=True)
     activation_key = models.CharField(max_length=100, blank=True)
-    key_expires = models.DateField(default=datetime.strftime(
-        datetime.now()+timedelta(days=2), "%Y-%m-%d"), editable=False, max_length=20)
+    key_expires = models.TextField(default=datetime.now()+timedelta(days=2), editable=False, max_length=20)
 
     def __str__(self):
         return f'{self.user_account_name.username} Profile'
