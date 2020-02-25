bc195109b8dae256c6d2c8d1f7c9983a33b65495
lisianthus <lisianthus623@gmail.com>
2020-02-25 14:44:28 +0700

    adding auth-token automation

--- backend/account/automation.py
+++ backend/account/automation.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+from rest_framework.authtoken.models import Token
+import time
 
 """
 when user has been save then send this signals
@@ -18,7 +20,8 @@ def create_profile(sender, instance, created, **kwargs):
     :param created:
     """
     if created:
-        Profile.objects.create(user_account_name=instance)
+        if User.objects.get(pk=instance.id):
+            Profile.objects.create(user_account_name=instance)
 
 
 @receiver(post_save, sender=User)
@@ -30,3 +33,7 @@ def save_profile(sender, instance, **kwargs):
     :param instance => User:
     """
     instance.profile.save()
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance, **kwargs):
+    Token.objects.get_or_create(user=instance)
