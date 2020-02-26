b7135c220c1da08ae41a72238d6c4731a24aa039
lisianthus <lisianthus623@gmail.com>
2020-02-26 20:56:24 +0700

    generate uniq-id for user-id

---
+++ backend/account/services/generate.py
@@ -0,0 +1,27 @@
+import hashlib
+import random
+
+
+def set_user_id(number):
+    salt = hashlib.sha256(str(random.random()).encode('utf-8')).hexdigest()[:8]
+    set_id = str(number * 264782)
+
+    user_id = str(salt[0:4]) + set_id
+    return user_id
+
+
+def get_real_user_id(number):
+    get_id = number[4:None]
+    set_id = int(get_id) / 264782
+
+    user_id = int(set_id)
+    return user_id
+
+x = 102
+
+SET_ID = set_user_id(x)
+GET_ID = get_real_user_id(SET_ID)
+
+print(f"{x} : {type(x)}")
+print(f"{SET_ID} : {type(SET_ID)}")
+print(f"{GET_ID} : {type(GET_ID)}")
