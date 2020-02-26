61bdc50d0b756af4760323fa4c6e945b096f2d5c
lisianthus <lisianthus623@gmail.com>
2020-02-26 20:53:47 +0700

    change input-form-fields

--- backend/core/forms.py
+++ backend/core/forms.py
@@ -9,17 +9,25 @@ PAYMENT_CHOICE = (
 
 
 class CheckoutForm(forms.Form):
-    street_address   = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': '1125 Main St'
-    }))
-    apartment_address =forms.CharField(required=False, widget=forms.TextInput(attrs={
-        'placeholder': 'Apartment Or Suite'
-    }))
-    country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
-        'class':'custom-select d-block w-100'
-    }))
+    street_address = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': '1125 Main St',
+            'class':'form-control'
+        }))
+    apartment_address = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Apartment Or Suite',
+            'class':'form-control'
+        }))
+    country = CountryField(
+        blank_label='( select country )').formfield(
+            widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100 form-control'
+        }))
+
     billing_zip = forms.CharField()
-    same_shipping_address =forms.BooleanField(widget=forms.CheckboxInput())
-    save_info= forms.BooleanField(widget=forms.CheckboxInput())
+    same_shipping_address = forms.BooleanField(widget=forms.CheckboxInput())
+    save_info = forms.BooleanField(widget=forms.CheckboxInput())
     payment_options = forms.ChoiceField(
         widget=forms.RadioSelect, choices=PAYMENT_CHOICE)
