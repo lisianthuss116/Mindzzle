50eb84c7a85ee07a53a93336c2da34e30688f308
lisianthus <lisianthus623@gmail.com>
2020-02-26 20:48:50 +0700

    update urls

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import patterns, url
-from . import views
+from django.urls import path
+from django.conf.urls import url
+from account import views
 
+app_name = 'account'
 
 urlpatterns = [
-    url(r'profile/(?P<username>[a-zA-Z0-9]+)$', views.get_user_profile),
+    # url(r'profile/(?P<username>[a-zA-Z0-9]+)$', views.get_user_profile),
 ]
