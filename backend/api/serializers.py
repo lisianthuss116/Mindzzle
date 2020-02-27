3f05d5ca465e498f42bc310d6f095c00e2fa2038
lisianthus <lisianthus623@gmail.com>
2020-02-27 09:33:48 +0700

    change serializer name

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from core.models import Item
 
-class ItemsSerializer(serializers.HyperlinkedModelSerializer):
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Item
         fields = [
