0da90387f48015d724355169df3afbd67c75b9c5
lisianthus <lisianthus623@gmail.com>
2020-02-27 16:03:39 +0700

    adding basename for api-route

--- Ecommerce/api_routers.py
+++ Ecommerce/api_routers.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from backend.api.viewsets import item_viewset
 
 router = routers.DefaultRouter()
-router.register(r'items', item_viewset.ItemView)
+router.register(r'items', item_viewset.ItemView, basename='items')
