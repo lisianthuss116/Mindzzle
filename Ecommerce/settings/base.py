ae30187e22ef3f7a9dce9aee0bc59fa52f15fce9
lisianthus <lisianthus623@gmail.com>
2020-02-27 09:33:28 +0700

    adding pagination in api-view

--- Ecommerce/settings/base.py
+++ Ecommerce/settings/base.py
@@ -35,9 +35,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
 }
 
 MIDDLEWARE = [
@@ -89,6 +86,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 20,
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+}
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Jakarta'
