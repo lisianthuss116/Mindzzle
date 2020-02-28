6b15949d9a228829e682fe3ba19abcfdbccfba16
lisianthus <lisianthus623@gmail.com>
2020-02-28 21:06:20 +0700

    deleted

--- backend/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from account import views
-
-app_name = 'account'
-
-urlpatterns = [
-    # url(r'profile/(?P<username>[a-zA-Z0-9]+)$', views.get_user_profile),
-]
