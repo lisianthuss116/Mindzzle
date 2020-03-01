0656c7db298ec2137a52d3f86916b985c4b7749d
lisianthus <lisianthus623@gmail.com>
2020-03-01 20:51:16 +0700

    adding commentary

--- backend/account/services/generate.py
+++ backend/account/services/generate.py
@@ -3,16 +3,21 @@ import random
 
 
 def set_user_id(number):
+    # generate uniq-str
     salt = hashlib.sha256(str(random.random()).encode('utf-8')).hexdigest()[:8]
-    set_id = str(number * 264782)
+    # create fake-user-id
+    set_id = str(number * 26845)
 
-    user_id = str(salt[0:4]) + set_id
+    # set uniq-str 4-char in front and set the user-id in the-rest
+    user_id = str(salt[0:6]) + set_id
     return user_id
 
 
 def get_real_user_id(number):
-    get_id = number[4:None]
-    set_id = int(get_id) / 264782
+    # get user-id after 4-character-in-the-front
+    get_id = number[6:None]
+    # get real-user-id
+    set_id = int(get_id) / 26845
 
     user_id = int(set_id)
     return user_id
